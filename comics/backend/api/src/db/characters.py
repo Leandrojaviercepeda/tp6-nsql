2946439a5f9a3c2a9f54e7356c4164740f3e5a03
Exequiel <exe.gonzalez14@hotmail.com>
2020-04-23 14:56:12 -0300

    up characters

--- comics/backend/api/src/db/characters.py
+++ comics/backend/api/src/db/characters.py
@@ -1,19 +1,163 @@
 characters = [{
         "id_character":"1",
-        "name": "Clark Joseph Kent",
-        "character_name": "Superman",
-        "year of appearance": "1938",
-        "house": "dc",
-        "biography": "Puede volar",
+        "name": "Hank Pym",
+        "character_name": "Ant Man",
+        "year of appearance": "1962",
+        "house": "Marvel",
+        "biography": "El Hombre Hormiga original fue el doctor Henry 'Hank' Pym, experto en biofísica y miembro del Centro de Operaciones de Seguridad que decidió convertirse en un superhéroe después de la muerte de su primera esposa, Maria Trovaya, quien había sido disidente política en su natal Hungría. Enamorada de él y creyendo que su ciudadanía estadounidense la protegería, Maria se casó con Hank y, poco después, viajó con él a Hungría, para comenzar una nueva vida juntos. Desafortunadamente, fueron confrontados por agentes corruptos de la policía secreta. Hank quedó inconsciente y María fue asesinada. Pym estaba muy angustiado por la muerte de su esposa, y decidió hacer todo lo posible en el futuro para luchar contra la injusticia. Después de descubrir una sustancia química que él llamó partículas Pym, que permitiría al usuario modificar su tamaño, se armó con un casco con el cual podía controlar a las hormigas. Después de eso, Pym se reduciría al tamaño de un insecto y se convertiría en la solución misteriosa de Ant-Man para resolver crímenes y detener a los criminales.",
         "equipment": "",
-        "amount_images": "4"
+        "amount_images": "1"
     },{
         "id_character":"2",
         "name": "Steven Grant Rogers",
-        "character_name": "Capitan America",
+        "character_name": "Capitán América",
         "year of appearance": "1941",
-        "house": "marvel",
-        "biography": "TIene un escudo",
+        "house": "Marvel",
+        "biography": "El Capitán América viste un traje que lleva un motivo de la bandera de los Estados Unidos, y utiliza un escudo casi indestructible que lanza como proyectil. El personaje es generalmente representado como el álter ego de Steve Rogers, un joven frágil mejorado a la cima de la perfección humana por un suero experimental para ayudar a los esfuerzos inminentes del gobierno de Estados Unidos en la Segunda Guerra Mundial. Cerca del final de la guerra, queda atrapado en el hielo y sobrevive en animación suspendida hasta que es descongelado en el presente. A pesar de que el Capitán América a menudo lucha por mantener sus ideales como un hombre fuera de su tiempo con sus realidades modernas, sigue siendo una figura muy respetada en su comunidad, hasta convertirse en el líder de Los Vengadores.",
         "equipment": "Escudo de vibranium",
+        "amount_images": "1"
+    },{
+        "id_character":"3",
+        "name": "T'Challa",
+        "character_name": "Pantera Negra",
+        "year of appearance": "1966",
+        "house": "Marvel",
+        "biography": "Es el rey y protector de la nación africana de Wakanda. Además de poseer habilidades mejoradas logradas a través de rituales antiguos de Wakanda de beber la esencia de la hierba en forma de corazón, T'Challa también confía en su dominio de la ciencia, el entrenamiento físico riguroso, las habilidades de combate mano a mano y el acceso a la riqueza y la avanzada tecnología de Wakanda para combatir a sus enemigos.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"4",
+        "name": "Claire Voyant",
+        "character_name": "Viuda Negra",
+        "year of appearance": "1940",
+        "house": "Marvel",
+        "biography": "Claire Voyant es una médium espiritual que se comunica con los muertos a través de medios sobrenaturales. Mientras servía a una familia llamada Waglers, Satanás la posee para maldecirlos. James, un miembro de esa familia, sobrevive a un posterior accidente automovilístico provocado por el hechizo y, al regresar a las habitaciones de Claire, la mata. El alma de Voyant va al infierno, donde Satanás la viste con su disfraz de Viuda Negra. Él también le da el poder de matar con un solo toque de sus dedos en la cabeza (que deja una marca de Marca de la Viuda Negra) y otros trucos místicos.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"5",
+        "name": "Stephen Strange",
+        "character_name": "Doctor Strange",
+        "year of appearance": "1963",
+        "house": "Marvel",
+        "biography": "Fue un cirujano brillante pero egoísta. Después de que un accidente automovilístico dañara gravemente sus manos y obstaculiza su capacidad para realizar una cirugía, busca una forma de repararlas encontrándose con el Anciano. Después de convertirse en uno de los alumnos del Hechicero Supremo, se convierte en un practicante tanto de las artes místicas como de las artes marciales. Además de conocer muchos hechizos poderosos, tiene un traje con dos objetos místicos: la Capa de Levitación y el Ojo de Agamotto, que le otorgan poderes adicionales.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"6",
+        "name": "Samuel Thomas Sam Wilson",
+        "character_name": "Falcon",
+        "year of appearance": "1969",
+        "house": "Marvel",
+        "biography": "Samuel Thomas Wilson nació en Harlem, Nueva York, como el hijo de Paul Wilson, un ministro protestante, y su madre Darlene Wilson. Sam tuvo una infancia feliz y descubrió que tenía una afinidad natural por las aves. Comenzó a entrenar palomas, teniendo el mayor palomar en Harlem. En su adolescencia, sin embargo, sus encuentros con el racismo lo dejaron cansado. Como el superhéroe Falcon, Wilson usa alas mecánicas para volar, y tiene un control telepático y empático limitado sobre las aves. Tras la jubilación de Steve Rogers, Wilson se convierte en el nuevo Capitán América y líder de los Vengadores.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"7",
+        "name": "Gamora Zen Whoberi Ben Titan",
+        "character_name": "Gamora",
+        "year of appearance": "1975",
+        "house": "Marvel",
+        "biography": "Gamora es la última de su especie, los Zen Whoberi, quienes fueron exterminados por los Badoon (en su línea temporal original, su especie fue exterminada por la Iglesia Universal de la Verdad). Thanos encontró a la niña y decidió utilizarla como un arma. Gamora fue criada y entrenada por Thanos para asesinar a Magus, una malvada versión alterna de Adam Warlock. Thanos mostró su poca humanidad en su infancia, pero Gamora fue muy leal al hombre que le prometió la oportunidad de vengar la muerte de su familia. Gamora pronto dominó las artes marciales, ganándose el apodo de la mujer más letal en toda la galaxia.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"8",
+        "name": "Clinton Francis Clint Barton",
+        "character_name": "Ojo de Halcón",
+        "year of appearance": "1964",
+        "house": "Marvel",
+        "biography": "Clint Barton nació en Waverly, Iowa. A una edad joven perdió a sus padres en un accidente automovilístico. Después de seis años en un orfanato, Clint y su hermano Barney se escaparon para unirse al Carnaval Viajero de Maravillas Carson. Clint pronto llamó la atención del Espadachín, que tomó al niño como su asistente. Junto con la ayuda de Trick Shot, el espadachín entrenador, Clint decide convertirse en un maestro arquero.",
+        "equipment": "Arco y flechas",
+        "amount_images": "1"
+    },{
+        "id_character":"9",
+        "name": "Robert Bruce Banner",
+        "character_name": "Hulk",
+        "year of appearance": "1962",
+        "house": "Marvel",
+        "biography": "Durante la detonación experimental de una bomba gamma, el científico Robert Bruce Banner salva al adolescente Rick Jones, que ha conducido al campo de pruebas; Banner empuja a Jones a una trinchera para salvarlo, pero es golpeado por la explosión, absorbiendo enormes cantidades de radiación gamma. Despierta más tarde, aparentemente ileso, pero esa noche se transforma en una forma gris y pesada. Un soldado perseguidor llama a la criatura Hulk.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"10",
+        "name": "Anthony Edward Stark",
+        "character_name": "Iron Man",
+        "year of appearance": "1963",
+        "house": "Marvel",
+        "biography": "Anthony Edward Stark, más conocido como Tony Stark, un multimillonario magnate empresarial estadounidense, playboy e ingenioso científico, sufre una grave lesión en el pecho durante un secuestro. Cuando sus captores intentan forzarlo a construir un arma de destrucción masiva crea, en cambio, una armadura poderosa para salvar su vida y escapar del cautiverio. Más tarde, Stark desarrolla su traje, agregando armas y otros dispositivos tecnológicos que diseñó a través de su compañía, Industrias Stark. Él usa el traje y las versiones sucesivas para proteger al mundo como Iron Man. Aunque al principio ocultó su verdadera identidad, Stark finalmente declaró que era, de hecho, Iron Man en un anuncio público.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"11",
+        "name": "Johann Schmidt",
+        "character_name": "Cráneo Rojo",
+        "year of appearance": "1941",
+        "house": "Marvel",
+        "biography": "Johann Schmidt era un exgeneral oficial nazi y confidente de Adolf Hitler. Ha estado íntimamente afiliado con Hydra y es enemigo de S.H.I.E.L.D., Los Vengadores, y otros intereses de los Estados Unidos y el mundo en general. Fue físicamente mejorado al traspasar su cerebro al cuerpo de un clon del Capitán América, que se encuentra en el pináculo de la perfección humana. Estuvo presuntamente muerto en el pasado, sólo para regresar al tiempo una y otra vez para plagar el mundo con esquemas de dominación global y genocidio.",
+        "equipment": "Cubo cósmico",
+        "amount_images": "1"
+    },{
+        "id_character":"12",
+        "name": "Peter Benjamin Parker",
+        "character_name": "Spider Man",
+        "year of appearance": "1962",
+        "house": "Marvel",
+        "biography": "Peter Benjamin Parker es un científico huérfano que vive con su tío Ben y su tía May. Es mordido por una araña radioactiva en una exhibición de ciencias y adquiere la agilidad y la fuerza proporcional de un arácnido. Junto con la súper fuerza, Parker gana la capacidad de adherirse a paredes y techos. A través de su habilidad nativa para la ciencia, desarrolla un dispositivo que le permite disparar cintas adhesivas de su propio diseño a través de pequeños barriles montados en la muñeca. Inicialmente buscando capitalizar sus nuevas habilidades, Parker se pone un disfraz y, como Spider-Man, se convierte en una estrella de televisión novedosa.",
+        "equipment": "Mecanismos artificiales en sus muñecas que disparan telarañas",
+        "amount_images": "1"
+    },{
+        "id_character":"13",
+        "name": "Peter Jason Quill",
+        "character_name": "Star Lord",
+        "year of appearance": "1976",
+        "house": "Marvel",
+        "biography": "Cuando por accidente la nave de J'son cae en la Tierra, él es rescatado por Meredith Quill. Los dos forman una relación, mientras J'son hace reparaciones a su nave. Eventualmente, J'son se ve obligado a salir para regresar a casa y luchar en una guerra. Se va, sin saber que Meredith está embarazada de Peter Quill. 10 años más tarde, Meredith es asesinada cuando es atacada por dos soldados Badoon que han venido a matar a Peter y terminar la línea de sangre de J'son. Peter los mata con una pistola, encuentra la pistola de su padre por accidente, y escapa de su casa antes de que sea destruida por la nave Badoon. Los Badoon presumen que Peter es asesinado y se va. Peter es colocado en un orfanato y finalmente se une a la NASA. Finalmente se explicó que fue criado por su madre Lisa Chang, que era comandante de la NASA.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"14",
+        "name": "Thanos The Titan",
+        "character_name": "Thanos",
+        "year of appearance": "1973",
+        "house": "Marvel",
+        "biography": "Thanos nació en el planeta Titán como el hijo de los Eternos Mentor (A'Lars) y Sui-San, y su hermano es Eros de Titán, también conocido como Starfox. Thanos lleva el gen Deviants, y como tal, comparte la apariencia física de la raza de los primos Eternos. Al nacer, su madre se sorprendió por su apariencia e intentó matarlo, debido a que creía que su hijo aniquilaría toda la vida en el universo, pero fue detenido por A'lars, el padre de Thanos. Durante sus años escolares, Thanos era un pacifista ​y solo jugaba con su hermano Eros y sus mascotas. En la adolescencia, Thanos se había fascinado con el nihilismo y la muerte, adorando y finalmente enamorándose de la encarnación física de la muerte, la señora Muerte. Como adulto, Thanos aumentó su fuerza física y sus poderes a través de su conocimiento científico superior. También intentó crear una nueva vida para sí mismo al engendrar muchos niños y convertirse en pirata. No encuentra ninguna satisfacción en ninguno de los dos hasta que es visitado nuevamente por Señora Muerte, por quien asesina a su descendencia y su capitán pirata.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"15",
+        "name": "Thor Odinson",
+        "character_name": "Thor",
+        "year of appearance": "1962",
+        "house": "Marvel",
+        "biography": "El padre de Thor, Odín, decide que su hijo necesita que le enseñe la humildad y, en consecuencia, coloca a Thor (sin recuerdos de la divinidad) en el cuerpo y los recuerdos de un estudiante de medicina humana existente, parcialmente discapacitado, Donald Blake. Después de convertirse en médico y de vacaciones en Noruega, Blake presencia la llegada de una partida de exploración alienígena. Blake huye de los extraterrestres a una cueva. Después de descubrir el martillo de Thor, Mjolnir (disfrazado como un bastón) y golpearlo contra una roca, se transforma en el dios del trueno.",
+        "equipment": "Martillo encantado Mjolnir",
+        "amount_images": "1"
+    },{
+        "id_character":"16",
+        "name": "Vision",
+        "character_name": "Visión",
+        "year of appearance": "1968",
+        "house": "Marvel",
+        "biography": "El robot Ultron es el creador de Visión, un tipo de androide que él llama un synthezoide, para usar contra el propio creador de Ultron, el Dr. Hank Pym (Ant-Man / Giant Man / Goliath / Yellowjacket) y la esposa de Pym, Janet van Dyne (la Avispa) del equipo de superhéroe Los Vengadores. Ultron envía a su nuevo sirviente para llevar a los Vengadores a una trampa. Avispa es la primera en encontrar el synthezoide, y lo describe como una visión mientras trata de escapar. Adoptando el nombre, los Vengadores convencen a la Visión para que se vuelva contra Ultron.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"17",
+        "name": "James Rupert Rhodes",
+        "character_name": "Máquina de Guerra",
+        "year of appearance": "1979",
+        "house": "Marvel",
+        "biography": "James Rhodey Rhodes, de la sección del sur de Filadelfia en Filadelfia, Pensilvania, era teniente en el Cuerpo de Marines de los Estados Unidos y se desempeñaba en misiones en el sudeste asiático. Piloto de combate, quedó varado en la jungla detrás de las líneas enemigas después de que su helicóptero fue derribado por los cohetes del Viet Cong. Se encuentra con Iron Man, que escapó del campo de prisioneros de Wong-Chu con su prototipo de armadura motorizada, por primera vez.",
+        "equipment": "",
+        "amount_images": "1"
+    },{
+        "id_character":"18",
+        "name": "James Buchanan Bucky Barnes",
+        "character_name": "Bucky Barnes",
+        "year of appearance": "1941",
+        "house": "Marvel",
+        "biography": "James Buchanan Barnes nació en Shelbyville, Indiana en 1917. ​Barnes creció como un mocoso del ejército. Él es un huérfano, hijo de un soldado asesinado en entrenamiento en el Campamento Lehigh del Ejército de los EE. UU. en Virginia justo antes de la entrada de Estados Unidos en la Segunda Guerra Mundial. Como resultado, se une al grupo de militares más como mascota que como un soldado. Apodado Bucky, viste un uniforme y se convierte en experto en los pormenores de la vida militar, a pesar de ser un adolescente. Fue en Lehigh que conoció y se hizo amigo del Soldado Steven Rogers, que según todas las apariencias es el más torpe soldado en el campo. Fue al mismo tiempo que ve cómo el entonces misterioso Capitán América comienza a aparecer en revistas de noticias, y Barnes devora con avidez las historias de este nuevo héroe.",
+        "equipment": "",
         "amount_images": "1"
     }]
