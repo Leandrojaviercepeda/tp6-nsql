51f58fe28e01e60ee577b23743481a332909c45e
Leandro <leandrojaviercepeda@gmail.com>
2020-04-22 22:45:51 -0300

    feat(api): characters list

---
+++ comics/backend/api/src/db/characters.py
@@ -0,0 +1,19 @@
+characters = [{
+        "id_character":"1",
+        "name": "Clark Joseph Kent",
+        "character_name": "Superman",
+        "year of appearance": "1938",
+        "house": "dc",
+        "biography": "Puede volar",
+        "equipment": "",
+        "amount_images": "4"
+    },{
+        "id_character":"2",
+        "name": "Steven Grant Rogers",
+        "character_name": "Capitan America",
+        "year of appearance": "1941",
+        "house": "marvel",
+        "biography": "TIene un escudo",
+        "equipment": "Escudo de vibranium",
+        "amount_images": "1"
+    }]
