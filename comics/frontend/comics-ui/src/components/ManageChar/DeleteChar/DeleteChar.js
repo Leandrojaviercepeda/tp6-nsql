f70fc9187885b6d8edaf17d033c91d8b780367b0
Leandro <leandrojaviercepeda@gmail.com>
2020-04-26 04:09:55 -0300

    feat(comics-ui): impementing delete char

--- comics/frontend/comics-ui/src/components/ManageChar/DeleteChar/DeleteChar.js
+++ comics/frontend/comics-ui/src/components/ManageChar/DeleteChar/DeleteChar.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import {ApiUrlBase} from '../../../utils/constants'
+import {useParams} from 'react-router'
+import CharSelector from '../../Character/CharSelector'
 
 export default function DeleteChar(props) {
+
+    const [characterSearched, setCharacterSearched] = useState('')
+    const handleCharacterSearched = char => setCharacterSearched(char)
+
     return (
         <div>
-            ¡Not Implemented!
+            <CharSelector 
+                handleCharacterSearched={handleCharacterSearched} 
+                characterSearched={characterSearched}
+            />
         </div>
     )
 }
