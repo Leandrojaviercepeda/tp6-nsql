be7b250836117cc852d79253e6d84f525c8d0305
Leandro <leandrojaviercepeda@gmail.com>
2020-04-28 23:47:14 -0300

    refactor(comics-ui): handle errors on CharSelector

--- comics/frontend/comics-ui/src/components/Character/CharSelector.js
+++ comics/frontend/comics-ui/src/components/Character/CharSelector.js
@@ -1,16 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router'
 
 import {AutoComplete} from 'primereact/autocomplete';
+import {ProgressSpinner} from 'primereact/progressspinner';
+import {Message} from 'primereact/message'
 
 import axios from 'axios'
 import {ApiUrlBase} from '../../utils/constants'
 
 export default function CharSelector(props) {
-    
-    let {house} = useParams()
-    const [error, setError] = useState({isError: false, message: ''})
-    const handleErrorMessage = message => setError({message: message, isError: true})
+
+    const [status, setStatus] = useState({showMessage: false, type: '', message:''})
+    const handleStatus = (showMessage, type='', message='') => setStatus({showMessage: showMessage, type: type, message: message})
 
     const [characters, setCharacters] = useState([])
     const handleCharacters = list => setCharacters(list)
@@ -19,43 +19,59 @@ export default function CharSelector(props) {
     const handleCharSugestions = chars => setCharSuggestions(chars)
 
     useEffect(() => {
-        console.log('Characters Render 1°');
         const fetchCharacters = async list => {
             try {
                 const charactersFetched = await axios.get(`${ApiUrlBase}/${list}`)
                 return charactersFetched.data 
-                    ? handleCharacters(charactersFetched.data.map(char => char.name))
-                    : handleErrorMessage('No hay personajes para mostrar')
+                    ? handleCharacters(charactersFetched.data.map(char => char.character_name))
+                    : handleStatus(true, 'error', 'No hay personajes para mostrar')
             } catch (error) {
-                handleErrorMessage('Ooops! Ha ocurrido un error al cargar la lista de Personajes')
+                handleStatus(true, 'error', 'Ooops! Ha ocurrido un error al cargar la lista de Personajes')
             }
         }
-        console.log(house)
-        fetchCharacters(house)
-    }, [house]);
+        fetchCharacters(props.house)
+    }, [props]);
 
-    const suggestChars = event => {        
+    const suggestChars = event => {
         if (event.query !== '')
             return handleCharSugestions([...characters].filter(char => char.toLowerCase().includes(event.query.toLowerCase())))
         return handleCharSugestions(characters)
     }
     
     return (
-        <div>
-            {
-                characters.lenght !== 0
-                ? 
-                    <AutoComplete 
-                        value={props.characterSearched} 
-                        suggestions={charSugestions}
-                        size={30} minLength={1}
-                        placeholder="Buscar..." 
-                        dropdown={true} 
-                        onChange={e => props.handleCharacterSearched(e.target.value)}
-                        completeMethod={e => suggestChars(e)}
-                    />
-                : null
-            }
+        <div className="charSelector">
+            <div className="p-grid p-justify-center m10">       
+                {
+                    status.showMessage 
+                    ? <Message severity={status.type} text={status.message}/>
+                    : null
+                }
+            </div>
+            <div className="p-grid p-justify-center m10">
+                {
+                    characters.lenght !== 0
+                    ? 
+                        <AutoComplete 
+                            value={props.characterSearched} 
+                            suggestions={charSugestions}
+                            size={30} 
+                            minLength={1}
+                            placeholder="Buscar..." 
+                            dropdown={true} 
+                            onChange={e => props.handleCharacterSearched(e.target.value)}
+                            completeMethod={e => suggestChars(e)}
+                        />
+                    : <ProgressSpinner 
+                            style={{
+                                position: 'absolute',
+                                top: '50%',
+                                left: '50%',
+                                marginRight: '-50%',
+                                transform: 'translate(-50%, -50%)',
+                            }}
+                        />
+                }
+            </div>
         </div>
     )
 }
