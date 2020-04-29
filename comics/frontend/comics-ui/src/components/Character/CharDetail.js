07af683a3435d3e342e139dd009a1228250c00e0
Leandro <leandrojaviercepeda@gmail.com>
2020-04-28 23:46:01 -0300

    refactor(comics-ui): adapting CharDetail to use with props and redirection

--- comics/frontend/comics-ui/src/components/Character/CharDetail.js
+++ comics/frontend/comics-ui/src/components/Character/CharDetail.js
@@ -1,15 +1,22 @@
 import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router'
+
+
+import {ProgressSpinner} from 'primereact/progressspinner'
+import {Panel} from 'primereact/panel';
+import {Message} from 'primereact/message';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import {Carousel} from 'react-responsive-carousel'
+
 import axios from 'axios'
 import {ApiUrlBase} from '../../utils/constants'
-import {useParams} from 'react-router'
-import {Carousel} from 'primereact/carousel'
-import {ProgressSpinner} from 'primereact/progressspinner'
 
-export default function CharDetail() {
+export default function CharDetail(props) {
 
-    let {id} = useParams()
-    const [error, setError] = useState({isError: false, message: ''})
-    const handleErrorMessage = message => setError({message: message, isError: true})
+    let {_id} = useParams()
+    const [status, setStatus] = useState({showMessage: false, type: '', message:''})
+    const handleStatus = (showMessage, type='', message='') => setStatus({showMessage: showMessage, type: type, message: message})
 
     const [character, setCharacter] = useState(null)
     const handleCharacter = character => setCharacter(character)
@@ -19,69 +26,89 @@ export default function CharDetail() {
         
         const fetchCharacter = async charId => {
             try {
-                const characterFetched = await axios.get(`${ApiUrlBase}/characters?id=${charId}`)
+                const characterFetched = await axios.get(`${ApiUrlBase}/characters?_id=${charId}`)
                 return characterFetched.data[0]
                     ? handleCharacter(characterFetched.data[0])
-                    : handleErrorMessage('No hay personaje para mostrar')
+                    : handleStatus(true, 'error', 'No hay personaje para mostrar :(')
             } catch (error) {
-                handleErrorMessage('Ooops! Ha ocurrido un error al cargar la lista de Personajes')
+                handleStatus(true, 'error' ,'Ooops! Ha ocurrido un error :(')
             }
-        }
-        fetchCharacter(id)
-    }, [id]);
-    
-
-    // charTemplate(char) {
-    //     return (
-    //         <div className="char-details">
-    //             <div className="p-grid p-nogutter">
-    //                 <div className="p-col-12">
-    //                     <img src={require(`../../utils/images/${character.house}/1.png`)} />
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     );
-    // };
+        }       
+        fetchCharacter(_id ? _id : props._id)
+    }, [_id, props]);
 
     return (
         <div className="charDetail">
-        {
-            character !== null
-            ?
-                <div className="p-grid p-justify-center m5">
-                    <div className="p-col"> 
-                    {/* <Carousel value={character} itemTemplate={this.charTemplate} orientation="vertical" style={{width: '400px', marginTop: '2em'}}
-                        numVisible={1} numScroll={1} responsive={this.responsiveSettings} verticalViewPortHeight="330px"></Carousel> */}
-                    </div>
+
+            <div className="p-grid p-justify-center m10">
+                {
+                    status.showMessage
+                    ? <Message severity={status.type} text={status.message}/>
+                    : null
+                }
+            </div>
+
+            {
+                character !== null
+                ?
+                    <div className="p-grid p-align-stretch vertical-container m10">
+
                     <div className="p-col">
-                        <div class="fancy">
-                            <div className="col-md-8 mx-auto ">
-                                <div className="p-col">
-                                    <h2 className="charName" style={{textAlign: 'center'}}>{character.character_name}</h2>
-                                    <hr class="style-three"/>
-                                    <h4 className="charNameH2">Nombre: {character.name}</h4>
-                                    <hr class="style-one"/>
-                                    <img height="50" width="65" src={require(`../../utils/images/Logos/${character.house}.png`)}/> 
-                                    <hr class="style-one"/>
-                                    <p className="charNameP">{`Equipamiento: ${character.equipment}`}</p>
-                                    <hr class="style-one"/>
-                                    <p className="charNameP">{character.biography}</p>
-                                </div>
-                            </div>
+                        <div className="box box-stretched">
+                            {/* Carousel of images */}
+                            <Carousel 
+                                showArrows={true}
+                                showThumbs={false}
+                                showIndicators={false}
+                                showStatus={false}
+                            >
+                            {
+                                character.images.length === 0
+                                ? 
+                                    <img 
+                                        src={require(`../../utils/images/Logos/${character.house}.png`)} 
+                                        key="1"
+                                        alt="img-1"
+                                        style={{ background: 'white' }}
+                                    />
+                                : 
+                                    character.images.map((img, i) => 
+                                        <img 
+                                            src={img} 
+                                            key={i+1} 
+                                            alt={`img-${i+1}`} 
+                                            style={{background: 'white'}}
+                                        />
+                                    )
+                            }
+                            </Carousel>
                         </div>
                     </div>
-                </div>
-        
-            : <ProgressSpinner 
-                    style={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        marginRight: '-50%',
-                        transform: 'translate(-50%, -50%)',
-                    }}
-                />
-        }
+                
+                    <div className="p-col">
+                        <Panel header={character.character_name} style={{textAlign: 'center'}}>
+                            <h4 className="charName">Nombre: {character.name}</h4>
+                            <hr/>
+                            <p>Casa: <span className={`fa fa-${character.house.toLowerCase()} m5`}style={{textAlign: 'center'}}/> </p>
+                            <hr/>
+                            <p className="charEquip">Equipamiento: { `${character.equipment.map(e => e)}`}</p>
+                            <hr/>
+                            <p className="charBiography" align="justify">{character.biography}</p>
+                        </Panel>
+                    </div>
+
+                    </div>
+            
+                : <ProgressSpinner 
+                        style={{
+                            position: 'absolute',
+                            top: '50%',
+                            left: '50%',
+                            marginRight: '-50%',
+                            transform: 'translate(-50%, -50%)',
+                        }}
+                    />
+            }
         </div>
     )
 }
