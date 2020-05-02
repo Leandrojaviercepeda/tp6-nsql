0c4036f214c7cd5460654d2469f2623d3bcce2d2
Exequiel <exe.gonzalez14@hotmail.com>
2020-05-02 00:15:40 -0300

    add json movies

---
+++ comics/backend/api/src/db/movies.py
@@ -0,0 +1,61 @@
+movies = [{
+        "poster_path": "/aGdng8Ic4ONpcepU3bVXPSLmNd3.jpg",
+        "id": 272,
+        "title": "Batman Begins",
+        "overview": "Driven by tragedy, billionaire Bruce Wayne dedicates his life to uncovering and defeating the corruption that plagues his home, Gotham City.  Unable to work within the system, he instead creates a new identity, a symbol of fear for the criminal underworld - The Batman.",
+        "release_date": "2005-06-10"
+    },{
+        "poster_path": "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+        "id": 475557,
+        "title": "Joker",
+        "overview": "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+        "release_date": "2019-10-02"
+    },{
+        "poster_path": "/5Kg76ldv7VxeX9YlcQXiowHgdX6.jpg",
+        "id": 297802,
+        "title": "Aquaman",
+        "overview": "Once home to the most advanced civilization on Earth, Atlantis is now an underwater kingdom ruled by the power-hungry King Orm. With a vast army at his disposal, Orm plans to conquer the remaining oceanic people and then the surface world. Standing in his way is Arthur Curry, Orm's half-human, half-Atlantean brother and true heir to the throne.",
+        "release_date": "2018-12-07"
+    },{
+        "poster_path": "/6ZYOpyhFZrAQIe1BuhqVgzfAdGZ.jpg",
+        "id": 1452,
+        "title": "Superman Returns",
+        "overview": "Superman returns to discover his 5-year absence has allowed Lex Luthor to walk free, and that those he was closest to felt abandoned and have moved on. Luthor plots his ultimate revenge that could see millions killed and change the face of the planet forever, as well as ridding himself of the Man of Steel.",
+        "release_date": "2006-06-28"
+    },{
+        "poster_path": "/lQUFeo0oRvtiLKlT5zkQkuCW4jA.jpg",
+        "id": 209112,
+        "title": "Batman v Superman: Dawn of Justice",
+        "overview": "Fearing the actions of a god-like Super Hero left unchecked, Gotham City’s own formidable, forceful vigilante takes on Metropolis’s most revered, modern-day savior, while the world wrestles with what sort of hero it really needs. And with Batman and Superman at war with one another, a new threat quickly arises, putting mankind in greater danger than it’s ever known before.",
+        "release_date": "2016-03-23"
+    },{
+        "poster_path": "/vSNxAJTlD0r02V9sPYpOjqDZXUK.jpg",
+        "id": 1771,
+        "title": "Captain America: The First Avenger",
+        "overview": "During World War II, Steve Rogers is a sickly man from Brooklyn who's transformed into super-soldier Captain America to aid in the war effort. Rogers must stop the Red Skull – Adolf Hitler's ruthless head of weaponry, and the leader of an organization that intends to use a mysterious device of untold powers for world domination.",
+        "release_date": "2011-07-22"
+    },{
+        "poster_path": "/4q2NNj4S5dG2RLF9CpXsej7yXl.jpg",
+        "id": 429617,
+        "title": "Spider-Man: Far from Home",
+        "overview": "Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.",
+        "release_date": "2019-06-28"
+    },{
+        "poster_path": "/gwi5kL7HEWAOTffiA14e4SbOGra.jpg",
+        "id": 284052,
+        "title": "Doctor Strange",
+        "overview": "After his career is destroyed, a brilliant but arrogant surgeon gets a new lease on life when a sorcerer takes him under her wing and trains him to defend the world against evil.",
+        "release_date": "2016-10-25"
+    },{
+        "poster_path": "/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+        "id": 299534,
+        "title": "Avengers: Endgame",
+        "overview": "After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.",
+        "release_date": "2019-04-24"
+    },{
+        "poster_path": "/54s3EKPVP4uIvKX4hj5X23ZbNYB.jpg",
+        "id": 76338,
+        "title": "Thor: The Dark World",
+        "overview": "Thor fights to restore order across the cosmos… but an ancient race led by the vengeful Malekith returns to plunge the universe back into darkness. Faced with an enemy that even Odin and Asgard cannot withstand, Thor must embark on his most perilous and personal journey yet, one that will reunite him with Jane Foster and force him to sacrifice everything to save us all.",
+        "release_date": "2013-10-29"
+    }]
