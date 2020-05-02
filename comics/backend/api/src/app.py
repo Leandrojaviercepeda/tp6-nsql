ed0f6f28bdb7f2ae4ee8574893b56405fd12c7bd
Leandro <leandrojaviercepeda@gmail.com>
2020-05-01 23:41:59 -0300

    refactor(api): endpoints for characters and movies

--- comics/backend/api/src/app.py
+++ comics/backend/api/src/app.py
@@ -17,30 +17,36 @@ def insert_superheores_by_default():
         return 'OK'
     except:
         raise
-    
-@app.route('/marvel', methods=['GET'])
-def marvel_characters():
-    """Retorna todos los personajes de Marvel"""
+
+@app.route('/movies', methods=['GET', 'DELETE', 'POST'])
+def movies_abm():
     try:
-        marvel_list_characters = list(db_comics.characters.find({'house':'Marvel'}))
-        for char in marvel_list_characters:
-            char['_id'] = str(char['_id'])
-        return jsonify(marvel_list_characters)
+        if request.method == 'DELETE':
+            # Borramos el dpersonaje de la coleccion
+            delete_result = db_comics.movies.delete_one({"_id": ObjectId(request.args['_id'])})
+            return 'ok'
+
+        if request.method == 'GET':
+
+            if request.args:
+                args = [k for k in dict(request.args).keys()]
+                movies = list(db_comics.movies.find({"_id": ObjectId(request.args['_id'])})) if args[0] == '_id' else list(db_comics.movies.find({args[0]: request.args[args[0]]}))
+                for movie in movies:
+                    movie['_id'] = str(movie['_id'])
+                return jsonify(movies)
+
+            movies = list(db_comics.movies.find({}))
+            for movie in movies:
+                movie['_id'] = str(movie['_id'])
+
+            return jsonify(movies)
+        
+        if request.method == 'POST':
+            # Insertamos el personaje en la coleccion
+            insert_result = db_comics.movies.insert_one(request.get_json())
+            return 'OK'
     except:
         raise
-
-
-@app.route('/dc', methods=['GET'])
-def dc_characters():
-    """Retorna todos los personajes de DC"""
-    try:
-        dc_list_characters = list(db_comics.characters.find({'house':'DC'}))
-        for char in dc_list_characters:
-            char['_id'] = str(char['_id'])
-        return jsonify(dc_list_characters)
-    except:
-        raise
-
 
 @app.route('/characters', methods=['GET', 'DELETE', 'POST', 'PUT'])
 def characters_abm():
@@ -51,11 +57,18 @@ def characters_abm():
             return 'ok'
 
         if request.method == 'GET':
-            # Obtenemos el personaje de la coleccion por id
-            args = [k for k in dict(request.args).keys()]
-            characters = list(db_comics.characters.find({"_id": ObjectId(request.args['_id'])})) if args[0] == '_id' else list(db_comics.characters.find({args[0]: request.args[args[0]]}))
+
+            if request.args:
+                args = [k for k in dict(request.args).keys()]
+                characters = list(db_comics.characters.find({"_id": ObjectId(request.args['_id'])})) if args[0] == '_id' else list(db_comics.characters.find({args[0]: request.args[args[0]]}))
+                for character in characters:
+                    character['_id'] = str(character['_id'])
+                return jsonify(characters)
+
+            characters = list(db_comics.characters.find({}))
             for character in characters:
                 character['_id'] = str(character['_id'])
+
             return jsonify(characters)
         
         if request.method == 'POST':
