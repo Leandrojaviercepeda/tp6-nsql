8fbfc4ee83362f527bb3eee2206163f672873bc7
Exequiel <exe.gonzalez14@hotmail.com>
2020-05-05 04:14:31 -0300

    add new movie

--- comics/frontend/comics-ui/src/components/ManageMovie/NewMovie/NewMovie.js
+++ comics/frontend/comics-ui/src/components/ManageMovie/NewMovie/NewMovie.js
@@ -1,26 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router'
+import MovieSearcher from '../../Movie/MovieSearcher'
 
 import {ProgressSpinner} from 'primereact/progressspinner'
 import {Panel} from 'primereact/panel';
 import {Message} from 'primereact/message';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import {Carousel} from 'react-responsive-carousel'
-import {InputText} from 'primereact/inputtext';
-import {Button} from "primereact/button";
 
+import axios from 'axios'
 import {ApiUrlBase} from '../../../utils/constants'
-import {TheMovieDBUrlBase} from '../../../utils/constants'
-import {TheMovieDB} from '../../../utils/credentials'
-import capitalize from '../../../utils/capitalize'
 
 export default function NewMovie() {
   
     const [status, setStatus] = useState({showMessage: false, type: '', message:''})
     const handleStatus = (showMessage, type='', message='') => setStatus({showMessage: showMessage, type: type, message: message})
 
-    const [movieSearched, setMovieSearched] = useState('')
-    const handleMovieSearched = list => setMovieSearched(list)
+    const handleMovieSearched = async movieSearch => {
+        try {
+            return await axios.post(`${ApiUrlBase}/movies`, movieSearch[0])
+                .then(handleStatus(true, 'success', 'Pelicula guardada exitosamente! :)'))
+                .then(setInterval(() => handleStatus(false), 5000))
+        } catch (error) {
+            handleStatus(true, 'error', '¡Ooops, ha ocurrido un error!')
+        }
+    }
 
     return (
         <div className="newMovie">
@@ -33,8 +37,6 @@ export default function NewMovie() {
                 }
             </div>
             {
-                // movie !== null
-                // ?
                     <div className="p-grid p-align-stretch vertical-container m10">
 
                     <div className="p-col">
@@ -47,15 +49,6 @@ export default function NewMovie() {
                                 showStatus={false}
                             >
                             {
-                                // movie.poster_path !== ''
-                                // ?   
-                                //     <img 
-                                //         src={`${TheMovieDBImagesUrlBase}${movie.poster_path}`} 
-                                //         key={movie.id} 
-                                //         alt={`img`} 
-                                //         style={{background: 'white'}}
-                                //     />
-                                // :
                                     <img 
                                         src={require(`../../../utils/images/Logos/Cinema.png`)} 
                                         key="1"
@@ -69,23 +62,11 @@ export default function NewMovie() {
                 
                     <div className="p-col">
                         <Panel header={'Buscador de peliculas'} style={{textAlign: 'center'}}>
-                            {/* <p className="charName">Año de lanzamiento: {movie.release_date}</p>
-                            <hr/>
-                            <p className="charBiography" align="justify">{movie.overview}</p>
-                            <hr/> */}
+                            <MovieSearcher handleMovieSearched={handleMovieSearched}/>
                         </Panel>
                     </div>
                 </div>
-            
-                // : <ProgressSpinner 
-                //         style={{
-                //             position: 'absolute',
-                //             top: '50%',
-                //             left: '50%',
-                //             marginRight: '-50%',
-                //             transform: 'translate(-50%, -50%)',
-                //         }}
-                //     />
+        
             }
         </div>
     )
--- comics/frontend/comics-ui/src/components/Movie/MovieSearcher.js
+++ comics/frontend/comics-ui/src/components/Movie/MovieSearcher.js
@@ -1,31 +1,72 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import {InputText} from 'primereact/inputtext';
-import capitalize from '../../utils/capitalize'
+import {Button} from 'primereact/button';
+import {AutoComplete} from 'primereact/autocomplete';
+
+import axios from 'axios'
+import {TheMovieDBUrlBase} from '../../utils/constants'
+import {TheMovieDB} from '../../utils/credentials'
 
 export default function MovieSearcher(props) {
     
+    const [status, setStatus] = useState({showMessage: false, type: '', message:''})
+    const handleStatus = (showMessage, type='', message='') => setStatus({showMessage: showMessage, type: type, message: message})
+
+    const [movies, setMovies] = useState([])
+    const handleMovies = list => setMovies(list)
+
     const [searchMovie, setSearchMovie] = useState('')
     const handleMovie = searchMovie => setSearchMovie(searchMovie)
+
+    const [moviesSugestions, setMoviesSuggestions] = useState([])
+    const handleMoviesSugestions = movies => setMoviesSuggestions(movies)
+
+    const [moviesSugestionsFilter, setMoviesSuggestionsFilter] = useState([])
+    const handleMoviesSugestionsFilter = movies => setMoviesSuggestionsFilter(movies)
 
     const handleSubmit = async e => {
         try {
             e.preventDefault()
-            console.log(searchMovie)
-            console.log('se envio')      
+            const moviesFetched = await axios.get(`${TheMovieDBUrlBase}/search/movie?api_key=${TheMovieDB}&query=${searchMovie}`)
+            const moviesList = moviesFetched.data.results
+            handleMovies(moviesList)
+            handleMoviesSugestions(moviesList.map(movie => movie.title))
         } catch (error) {
             handleStatus(true, 'error', '¡Ooops, ha ocurrido un error!')
         }
     }
 
+    const sendMovie = async e => {
+        e.preventDefault()
+        return props.handleMovieSearched(movies.filter(movie => movie.title===searchMovie))
+    }
+
+    const suggestMovies = event => {
+        if (event.query !== '')
+            return handleMoviesSugestionsFilter([...moviesSugestions].filter(movie => movie.toLowerCase().includes(event.query.toLowerCase())))
+        return handleMoviesSugestionsFilter(moviesSugestions)
+    }
+
     return (
         <div className="movieSearcher">
 
-            <div className="p-col-12 p-md-4">
+            <div>
                 <h3>Ingrese una pelicula</h3>
-                    <div className="p-inputgroup">
-                        <InputText value={searchMovie} onChange={(e) => handleMovie(capitalize(e.target.value))} />
-                        <Button label="Buscar" onClick={handleSubmit}/>
+                    <div>
+                        <Button icon="pi pi-search" className="p-button-secondary" onClick={handleSubmit}/>
+                        <AutoComplete 
+                            value={searchMovie} 
+                            suggestions={moviesSugestionsFilter}
+                            size={30} 
+                            minLength={1}
+                            placeholder="Buscar..." 
+                            dropdown={true} 
+                            onChange={e => handleMovie(e.target.value)}
+                            completeMethod={e => suggestMovies(e)}
+                        />
+                    </div>  
+                    <div style={{marginTop: '15px'}}>
+                        <Button icon="pi pi-save" label="Añadir" className="p-button-secondary" onClick={sendMovie}/>
                     </div>
             </div>
         </div>
