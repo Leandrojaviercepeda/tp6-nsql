c48b7d6a9e9da6decfcd3c4224bb6e42ebd40a87
Leandro <leandrojaviercepeda@gmail.com>
2020-05-06 00:09:34 -0300

    feat(comics-ui): Movie

--- comics/frontend/comics-ui/src/components/Movie/MovieCard.js
+++ comics/frontend/comics-ui/src/components/Movie/MovieCard.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-export default function MovieCard() {
+import {Card} from 'primereact/card';
+
+import {TheMovieDBImagesUrlBase} from '../../utils/constants'
+
+export default function CharCard(props) {
+
+    const header = (
+        <img alt="Card" src={`${TheMovieDBImagesUrlBase}${props.movie.poster_path}`}/>
+    );
+    const footer = (
+        <span>
+            <a className="detail" href={`/movies/${props.movie._id}`}>Detalle</a>
+        </span>
+    );
     return (
-        <div>
-            Not Implemented!
-        </div>
+        <Card 
+            title={props.movie.title} 
+            subTitle={`${props.movie.title} (${props.movie.release_date})`} 
+            style={{maxWidth: '280px', minWidth: '250px'}} 
+            className="ui-card-shadow m5" 
+            header={header}
+            footer={footer} 
+        >
+            <div>
+                <p
+                style={{
+                    width: '200px',
+                    whiteSpace: 'nowrap',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                }}
+                >
+                    {props.movie.overview}
+                </p>
+            </div>
+        </Card>
     )
 }
--- comics/frontend/comics-ui/src/components/Movie/MovieSelector.js
+++ comics/frontend/comics-ui/src/components/Movie/MovieSelector.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-export default function MovieSelector() {
+import {AutoComplete} from 'primereact/autocomplete';
+import {ProgressSpinner} from 'primereact/progressspinner';
+import {Message} from 'primereact/message'
+
+import axios from 'axios'
+import {ApiUrlBase} from '../../utils/constants'
+
+export default function MovieSelector(props) {
+
+    const [status, setStatus] = useState({showMessage: false, type: '', message:''})
+    const handleStatus = (showMessage, type='', message='') => setStatus({showMessage: showMessage, type: type, message: message})
+
+    const [movies, setMovies] = useState([])
+    const handleMovies = list => setMovies(list)
+
+    const [moviesSugestions, setMoviesSuggestions] = useState([])
+    const handleMoviesSugestions = movies => setMoviesSuggestions(movies)
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const moviesFetched = await axios.get(`${ApiUrlBase}/movies`)
+                return moviesFetched.data
+                    ? handleMovies(moviesFetched.data.map(movie => movie.title))
+                    : handleStatus(true, 'error', 'No hay personajes para mostrar')
+            } catch (error) {
+                handleStatus(true, 'error', 'Ooops! Ha ocurrido un error al cargar la lista de Personajes')
+            }
+        }
+        fetchMovies()
+    }, []);
+
+    const suggestMovies = event => {
+        if (event.query !== '')
+            return handleMoviesSugestions([...movies].filter(movie => movie.toLowerCase().includes(event.query.toLowerCase())))
+        return handleMoviesSugestions(movies)
+    }
+    
     return (
-        <div>
-            Not Implemented!
+        <div className="movieSelector">
+            <div className="p-grid p-justify-center m10">       
+                {
+                    status.showMessage 
+                    ? <Message severity={status.type} text={status.message}/>
+                    : null
+                }
+            </div>
+            <div className="p-grid p-justify-center m10">
+                {
+                    movies.lenght !== 0
+                    ? 
+                        <AutoComplete 
+                            value={props.movieSearched} 
+                            suggestions={moviesSugestions}
+                            size={30} 
+                            minLength={1}
+                            placeholder="Buscar..." 
+                            dropdown={true} 
+                            onChange={e => props.handleMovieSearched(e.target.value)}
+                            completeMethod={e => suggestMovies(e)}
+                        />
+                    : <ProgressSpinner 
+                            style={{
+                                position: 'absolute',
+                                top: '50%',
+                                left: '50%',
+                                marginRight: '-50%',
+                                transform: 'translate(-50%, -50%)',
+                            }}
+                        />
+                }
+            </div>
         </div>
     )
 }
