719c6f88d2caadcb920f47a070a42ded9f89d965
Leandro <leandrojaviercepeda@gmail.com>
2020-05-06 04:05:06 -0300

    refactor(comics-ui):  MovieSearcher

--- comics/frontend/comics-ui/src/components/Movie/MovieSearcher.js
+++ comics/frontend/comics-ui/src/components/Movie/MovieSearcher.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {Button} from 'primereact/button';
 import {AutoComplete} from 'primereact/autocomplete';
@@ -15,8 +15,8 @@ export default function MovieSearcher(props) {
     const [movies, setMovies] = useState([])
     const handleMovies = list => setMovies(list)
 
-    const [searchMovie, setSearchMovie] = useState('')
-    const handleMovie = searchMovie => setSearchMovie(searchMovie)
+    const [movieSearched, setMovieSearched] = useState('')
+    const handleMovieSearched = movie => setMovieSearched(movie)
 
     const [moviesSugestions, setMoviesSuggestions] = useState([])
     const handleMoviesSugestions = movies => setMoviesSuggestions(movies)
@@ -24,21 +24,16 @@ export default function MovieSearcher(props) {
     const [moviesSugestionsFilter, setMoviesSuggestionsFilter] = useState([])
     const handleMoviesSugestionsFilter = movies => setMoviesSuggestionsFilter(movies)
 
-    const handleSubmit = async e => {
+    const searchMovies = async () => {
         try {
-            e.preventDefault()
-            const moviesFetched = await axios.get(`${TheMovieDBUrlBase}/search/movie?api_key=${TheMovieDB}&query=${searchMovie}`)
+            const moviesFetched = await axios.get(`${TheMovieDBUrlBase}/search/movie?api_key=${TheMovieDB}&query=${movieSearched}`)
             const moviesList = moviesFetched.data.results
             handleMovies(moviesList)
             handleMoviesSugestions(moviesList.map(movie => movie.title))
+            handleMovieSearched('')
         } catch (error) {
             handleStatus(true, 'error', '¡Ooops, ha ocurrido un error!')
         }
-    }
-
-    const sendMovie = async e => {
-        e.preventDefault()
-        return props.handleMovieSearched(movies.filter(movie => movie.title===searchMovie))
     }
 
     const suggestMovies = event => {
@@ -53,22 +48,24 @@ export default function MovieSearcher(props) {
             <div>
                 <h3>Ingrese una pelicula</h3>
                     <div>
-                        <Button icon="pi pi-search" className="p-button-secondary" onClick={handleSubmit}/>
+                        <Button icon="pi pi-search" className="p-button-secondary" onClick={searchMovies}/>
                         <AutoComplete 
-                            value={searchMovie} 
+                            value={movieSearched}
                             suggestions={moviesSugestionsFilter}
                             size={30} 
                             minLength={1}
                             placeholder="Buscar..." 
                             dropdown={true} 
-                            onChange={e => handleMovie(e.target.value)}
+                            onChange={e => handleMovieSearched(e.target.value)}
                             completeMethod={e => suggestMovies(e)}
+                            onSelect={e => props.handleMovieToSave(...movies.filter(movie => movie.title===e.value))}
                         />
                     </div>  
                     <div style={{marginTop: '15px'}}>
-                        <Button icon="pi pi-save" label="Añadir" className="p-button-secondary" onClick={sendMovie}/>
+                        <Button icon="pi pi-save" label="Añadir" className="p-button-secondary" onClick={() => props.saveMovie()}/>
                     </div>
             </div>
         </div>
     )
 }
+// 
